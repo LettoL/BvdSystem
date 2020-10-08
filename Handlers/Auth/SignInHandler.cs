5965166e6d4a3c4bee2f254940f64d02c7494fc0
Denis <letto-l@yandex.ru>
2020-10-08 21:31:45 +0300

    server auth

---
+++ Handlers/Auth/SignInHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Data;
+using Data.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Handlers.Auth
+{
+    public static class SignInHandler
+    {
+        public static async Task<User> Execute(PostgresContext db, string login, string password)
+        {
+            var user = await db.Users
+                .FirstOrDefaultAsync(x => x.Login == login && x.Password == password);
+            
+            if(user == null)
+                throw new Exception("Неверные логин или пароль");
+
+            return user;
+        }
+    }
+}
---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Data;
+using Handlers.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly PostgresContext _db;
+
+        public AuthController(PostgresContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var result = SignInHandler.Execute(_db, "", "");
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
